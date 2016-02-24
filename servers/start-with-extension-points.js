7a5ec03a5ed8293e104c523a13434a9595548af6
IndieForger <indieforger@gmail.com>
2016-02-24 17:38:38 +0000

    start server with extension points

---
+++ servers/start-with-extension-points.js
@@ -0,0 +1,33 @@
+'use strict'
+const
+  hapi = require('hapi'),
+  server = new hapi.Server()
+
+server.connection({
+	port: 3000,
+	host: 'localhost'
+})
+
+// Extension Event registration
+// ---------------------------------------------------
+// Change all requests to '/test'
+// Official docs
+// > http://hapijs.com/api#serverextevents
+// > http://hapijs.com/api#serverextevent-method-options
+// Lifecycle of a request graph:
+// > https://blog.risingstack.com/getting-started-with-hapi-8/
+server.ext('onRequest', (request, reply) => {
+	request.setUrl('/test')
+	return reply.continue()
+})
+
+server.route({
+	method: 'GET',
+	path: '/test',
+	handler: (request, reply) => reply('Hello world from test!')
+})
+
+server.start(function (err) {
+	if (err) throw err
+	console.log(`hapi server started on ${server.info.uri}`)
+})
