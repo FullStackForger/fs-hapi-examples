d715272736bbadb7b75c921d14e655151eb3925b
IndieForger <indieforger@gmail.com>
2016-02-24 17:59:17 +0000

    start-multiple.js

---
+++ servers/start-multiple.js
@@ -0,0 +1,37 @@
+'use strict'
+const
+  hapi = require('hapi'),
+  port = 3000,
+  server = new hapi.Server()
+
+// add first connection
+server.connection({
+    host: 'localhost',
+    port: process.env.PORT || port
+})
+
+// add second connection
+server.connection({
+    host: 'localhost',
+    port: process.env.PORT + 1 || port + 1
+})
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: (request, reply) => {
+    // check stackoverflow answer for building full URL
+    // http://stackoverflow.com/a/31841704
+    let url = request.connection.info.protocol
+      + '://' + request.info.host
+    reply(`Hell world from ${url}`)
+  }
+})
+
+// Start the server
+server.start(function () {
+    // Log to the console the host and port info
+    server.connections.forEach((connection) => {
+      console.log(`Server started at: ${connection.info.uri}`)
+    })
+})
