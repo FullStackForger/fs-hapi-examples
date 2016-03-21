5740330bff75508409ad8308042459954d120a87
IndieForger <indieforger@gmail.com>
2016-03-21 21:19:35 +0000

    mulitple-routes

---
+++ routes/mulitple-routes.js
@@ -0,0 +1,34 @@
+'use strict';
+const hapi = require('hapi');
+const server = new hapi.Server();
+
+const routes = [
+    { method: 'GET', path: '/a', handler: defaultHandler },
+    { method: 'GET', path: '/b', handler: defaultHandler }
+];
+
+const connection = {
+    port: 3000,
+    host: 'localhost'
+};
+
+server.connection(connection);
+server.route(routes);
+server.start(startHandler);
+
+function defaultHandler (request, reply) {
+    reply({
+        request: {
+            method: request.method,
+            info: request.info,
+            query: request.query,
+            path: request.path
+        },
+        url: request.connection.info.protocol + '://' + request.info.host + request.url.path
+    });
+}
+
+function startHandler (err) {
+    if (err) throw err;
+    console.log(`hapi server started on ${server.info.uri}`);
+}
