7393114a67f2b3388598acec5c68fbea565c8a2f
IndieForger <indieforger@gmail.com>
2016-03-22 03:34:41 +0000

    static-files/directory-listing.js

---
+++ static-files/directory-listing.js
@@ -0,0 +1,27 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const server = new Hapi.Server();
+const connection = {
+    host: 'localhost',
+    port: 3000
+};
+
+server.register(Inert, () => {});
+server.connection(connection);
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+        directory: {
+            //index: 'index.html',    // default set to: 'index.html'
+            //showHidden: false,      // default set to: false
+            path: 'public',
+            listing: true
+        }
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+    console.log(`hapi server started on ${server.info.uri}`);
+});
